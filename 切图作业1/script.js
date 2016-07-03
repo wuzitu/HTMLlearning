61bbb3f9c932a93c3e3dd0ca550d7c6defc476e9
wuzitu <56wulei56@gmail.com>
2016-07-03 20:13:01 +0800

    my first page on GitHub
    第一次提交git，好紧张~
    外包页面的新手练习。
    练习中成长，加油。
    
    just a simple page.
    a little practice for myself.
    hope months later could review this,and find that it's really terrible~

---
+++ 切图作业1/script.js
@@ -0,0 +1,38 @@
+
+console.log("asdf")
+// window.onload = function(){
+//   $(".imghover").hover(function(){ 
+// $(this).find("div").stop().fadeIn(); 
+// },function(){ 
+// $(this).find("div").stop().fadeOut(); 
+// }); 
+// };
+
+// window.onload = function(){
+// 	$("#more").click(){
+// 		function(){
+// 			$(this).fadeToggle("slow");
+// 		}
+// 	}
+// }
+
+// window.onload = function(){
+// 	$("#fixNuma").hover(
+// 		function(){
+// 			$("#fixNum").css("display","block");
+// 		}
+// 		,function(){
+// 			$("#fixNum").css("display","none");
+// 		}
+
+// 		)
+// }
+// 	
+
+window.onload = function(){
+$(".fixNuma").hover(
+function(){
+			$("#fixNum").fadeToggle("slow");
+		}
+	)
+}
